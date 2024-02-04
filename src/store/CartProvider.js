02114e43bc62045d8bd1a8a053d0506c7d3a957d
VaishnaviKulkarni111 <vaishnavikulkarni1822@gmail.com>
2024-02-04 17:24:28 +0530

    Axios added

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,40 +1,77 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import CartContext from './cart-context';
+import axios from 'axios';
 
 const CartProvider =(props) =>{
+  const url = "https://crudcrud.com/api/ee4f9ce5c51d4852a29ac2d1a629a28f/cart"
     const [items,updateItems] =useState([]);
 
-    const addItemHandler=(item)=>{
-        let repeatItem = items.find((newItem) => newItem.name === item.name);
-       
+    const addItemHandler= async(item) =>{
+      try{  
+        let repeatItem = items.find((newItem) => newItem.title === item.title);
+
         if (repeatItem === undefined) {
-          updateItems([...items, { ...item, quantity: Number(item.quantity) }]);
+          const response = await axios.post(url, {
+            ...item, quantity: Number(item.quantity),
+           });
+          updateItems([...items, response.data]);
         } else {
           repeatItem.quantity += Number(item.quantity);
-           updateItems([...items]);
+          await axios.put(`${url}/${repeatItem._id}`, {
+            quantity: repeatItem.quantity,
+          });
+          updateItems([...items]);
         }
+  }catch(err){
+       console.log(err)
+     }    
     }
 
-    const removeItemHandler=(item)=>{
-        const updatedItems = [...items];
+    const removeItemHandler=async (item)=>{
+      const updatedItems = [...items];
+    // try{
+    //   const ExistingItemIndex = updatedItems.findIndex(
+    //     (newItem) => newItem.name === item.name
+    //   );
+    //   if (ExistingItemIndex !== -1) {
+    //     if (updatedItems[ExistingItemIndex].quantity > 1) {
+    //       updatedItems[ExistingItemIndex].quantity -= 1;
+    //       await axios.put(
+    //         `${url}/${updatedItems[ExistingItemIndex]._id}`,
+    //         {
+    //           quantity: updatedItems[ExistingItemIndex].quantity,
+    //         });
+    //         updateItems(updatedItems);
+    //     } 
+    //     else {
+    //       await axios.delete(
+    //         `${url}/${updatedItems[ExistingItemIndex]._id}`
+    //       );
+    //       updatedItems.splice(ExistingItemIndex,1);
+    //     }
+    //     updateItems(updatedItems);
+    //   }
 
-        const ExistingItemIndex = updatedItems.findIndex(
-          (newItem) => newItem.name === item.name
-        );
-        if (ExistingItemIndex !== -1) {
-          if (updatedItems[ExistingItemIndex].quantity > 1) {
-            updatedItems[ExistingItemIndex].quantity -= 1;
-          } 
-          else {
-            updatedItems.splice(ExistingItemIndex,1);
-          }
-          updateItems(updatedItems);
-        }
+    // }catch(err){
+    //  console.log(err)
+    // }     
     }
 
     const resetCartHandler = () => {
       updateItems([]);
     }
+
+    const getItem = async () => {
+      try {
+        const response = await axios.get(url);
+         updateItems( response.data);
+      } catch (error) {
+        console.log("error fetching cart cart items", error);
+      }
+    };
+    useEffect(() => {
+      getItem();
+    }, []);
 
     const ListContext={
         items:items,
--- src/store/MedicineProvider.js
+++ src/store/MedicineProvider.js
@@ -1,18 +1,41 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import MedicineContext from './medicine-context';
+import axios from 'axios';
 
 const MedicineProvider = (props) => {
+  const url = "https://crudcrud.com/api/ee4f9ce5c51d4852a29ac2d1a629a28f/medicines"
     const [medicines, setMedicines] = useState([]);
 
-    const addMedicine = (newMedicine) => {
-        
-        setMedicines((prevmedicines) => [...prevmedicines, newMedicine]);
-      };
+    const addMedicine = async (newMedicine) => {
+    try{
+      const response = await axios.post(url, { ...newMedicine });
+      setMedicines([...medicines, response.data]);
+      
+      //setMedicines((prevmedicines) => [...prevmedicines, newMedicine]);
+
+    }catch(error){
+      console.log(error)
+    }         
+   };
 
       const medicineValue = {
         medicines,
         addMedicine,
       };
+
+      
+    const getItem = async () => {
+      try {
+        const response = await axios.get(url);
+         setMedicines( response.data);
+  
+      } catch (error) {
+        console.log("error fetching cart cart items", error);
+      }
+    };
+    useEffect(() => {
+      getItem();
+    }, []);
     
       return (
         <MedicineContext.Provider value={medicineValue}>
