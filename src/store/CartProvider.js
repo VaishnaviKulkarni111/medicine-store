cee1545de0ad704c38e578cb90c522d30ff5ed36
VaishnaviKulkarni111 <vaishnavikulkarni1822@gmail.com>
2024-04-23 15:24:00 +0530

    commit

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -3,84 +3,48 @@ import CartContext from './cart-context';
 import axios from 'axios';
 
 const CartProvider =(props) =>{
-  const  [items, setItems] = useState([]);
-  const url = "https://crudcrud.com/api/191a987ce4a84f1a91a278421f407f0c/cart"
-   
+  const url = "https://crudcrud.com/api/ebd1670a5cf047c9a846e7404a8de6c8/cart"
+  const  [items, updateItems] = useState([]);
+  console.log('items' ,items)
+  const addItemHandler = async (item) => {
+      let repeatItem = items.find((newItem) => newItem.name === item.name);
 
-    const addItemHandler = async (item) => {
-      try{  
-        let repeatItem = items.find((newItem) => newItem.title === item.title);
-
-        if (!repeatItem) {
-          const response = await axios.post(url, { ...item, quantity: Number(item.quantity)
-           });
-          
-          setItems([...items, response.data]);
-        } else {
-          repeatItem.quantity += Number(item.quantity);
-          await axios.put(`${url}/${repeatItem._id}`, {
-            quantity: repeatItem.quantity,
-          });
-          setItems([...items]);
-        }
-  }catch(err){
-       console.log(err)
-     }    
-    };
-
-    const removeItemHandler = async (item) => {
-        const updatedItems = [...items];
-try{
-  const foundItemIndex = updatedItems.findIndex(
-    (newItem) => newItem.title === item.title
-  );
-
-  if (foundItemIndex !== -1) {
-    if (updatedItems[foundItemIndex].quantity > 1) {
-      updatedItems[foundItemIndex].quantity -= 1;
-      await axios.put(
-        `${url}/${updatedItems[foundItemIndex]._id}`,
-        {
-          quantity: updatedItems[foundItemIndex].quantity,
-        }
-      );
-      setItems(updatedItems);
-    } 
-    else  {
-      await axios.delete(
-        `${url}/${updatedItems[foundItemIndex]._id}`
-      );
-      updatedItems.splice(foundItemIndex, 1);
-      setItems(updatedItems);
-    }
-  }
-  }catch(err){
-  console.log(err)
-  }       
-    };
-
-  
-    const getItem = async () => {
-      try {
-        const response = await axios.get(url);
-         setItems( response.data);
-      } catch (error) {
-        console.log( error);
+      if (repeatItem === undefined) {
+        updateItems([...items, { ...item, quantity: Number(item.quantity) }]);
+        const response = await axios.post(url, {
+          ...props.item.value, quantity: Number(item.quantity),
+         });
+         console.log('res', response)
+      } else {
+        repeatItem.quantity += Number(item.quantity);
+        updateItems([...items]);
       }
-    };
+  };
 
-    useEffect(() => {
-      getItem();
-    }, []);
-    function resetCartHandler(){
-      setItems([])
-    }
+  const removeItemHandler = (item) => {
+      const updatedItems = [...items];
+
+      const foundItemIndex = updatedItems.findIndex(
+        (newItem) => newItem.name === item.name
+      );
+  
+      if (foundItemIndex !== -1) {
+        if (updatedItems[foundItemIndex].quantity > 1) {
+          updatedItems[foundItemIndex].quantity -= 1;
+        } else {
+          updatedItems.splice(foundItemIndex, 1);
+        }
+  
+        updateItems(updatedItems);
+      }
+  };
+
 
     const ListContext={
         items:items,
         addItem:addItemHandler,
         removeItem:removeItemHandler, 
-        resetCart: resetCartHandler,
+        // resetCart: resetCartHandler,
     }
 
     return(
--- src/store/MedicineProvider.js
+++ src/store/MedicineProvider.js
@@ -3,7 +3,7 @@ import MedicineContext from './medicine-context';
 import axios from 'axios';
 
 const MedicineProvider = (props) => {
-  const url = "https://crudcrud.com/api/191a987ce4a84f1a91a278421f407f0c/medicines"
+  const url = "https://crudcrud.com/api/ebd1670a5cf047c9a846e7404a8de6c8/medicines"
     const [medicines, setMedicines] = useState([]);
     const addMedicine = async (newMedicine) => {
     try{
